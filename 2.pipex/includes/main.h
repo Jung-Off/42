ae7f099cbc8ca91731b2be948a124bf98e3ee72c
lewis941001 <lewis1004@naver.com>
2021-07-05 16:08:39 +0900

    [210705_header]

--- 2.pipex/includes/main.h
+++ 2.pipex/includes/main.h
@@ -32,9 +32,10 @@ typedef struct	s_exe
 	char * const	*envp;
 }				t_exe;
 
+int				make_outfile(char *outfile);
 void			*free_mem(char *const *mem);
 void			init_exe(t_exe *exe, char const *argv);
-void			connect_file_to_stdout(char *outfile);
+void			connect_file_to_stdout(int fd);
 void			connect_stdin_pipe_r(int pipefd[2]);
 void			use_pipe_r_to_file(char *argv, char *file);
 void			connect_file_to_stdin(const char *infile);
