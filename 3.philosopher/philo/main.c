5a69126aa87e5b3a96ee589e31ebf3ac62bb9493
lewis941001 <lewis1004@naver.com>
2021-09-10 18:48:31 +0900

    [210910_add]set_table

--- 3.philosopher/philo/main.c
+++ 3.philosopher/philo/main.c
@@ -12,9 +12,14 @@
 
 #include "main.h"
 
-void error_print(void)
+void error_print(int err_num)
 {
-	printf("error argc count\n");
+	if(err_num == 1)
+		printf("error argc count\n");
+	else if(err_num == 2)
+		printf("error input char\n");
+	else if(err_num == 3)
+		printf("error input outofrange\n");
 	exit(1);
 }
 
@@ -45,84 +50,128 @@ int		philo_isspace(char ch)
 
 int philo_atoi(char *str)
 {
-	int			sign;
+	int			isnum;
+	int			minus;
 	long long	num;
 
-	sign = 1;
+	isnum = 0;
+	minus = 1;
 	num = 0;
 	while (philo_isspace(*str))
 		str++;
 	if (*str == '-' || *str == '+')
 	{
 		if (*str == '-')
-			sign *= -1;
+			minus *= -1;
 		str++;
 	}
-	while (philo_isdigit(*str))
+	while (*str)
 	{
-		num *= 10;
-		num += (*(str++) - '0');
+		if(!philo_isdigit(*str))
+			error_print(2);
+		num = num * 10 + (*(str++) - '0');
+		isnum = 1;
 	}
-	return (num * sign);
+	num *= minus;
+	if (isnum == 0)
+		error_print(2);
+	else if(num > 2147483637 || num < -2147483648)
+		error_print(3);
+	return (num);
 }
 
-
-t_philo *philo_init(int ac, char *av[])
+t_data *data_init(int ac, char *av[])
 {
-	t_philo *philo;
-	t_status status[philo_atoi(av[1])];
+	t_data *data;
 
-	philo = (t_philo *)malloc(sizeof(t_philo));
+	//숫자로 만 입력받기
+	if(!(data = (t_data *)malloc(sizeof(t_data))))
+		return (0);
+	data->num_of_philo = philo_atoi(av[1]);
+	data->time_to_die = philo_atoi(av[2]);
+	data->time_to_eat = philo_atoi(av[3]);
+	data->time_to_sleep = philo_atoi(av[4]);
+	data->eat_count = philo_atoi(av[ac - 1]);
+	return (data);
+}
 	
+t_status *status_init(int ac, char *av[])
+{
+	t_data *data;
+	pthread_mutex_t *forks;
+	t_status *status;
+	
+	data = data_init(ac, av);
+	if(!(status = (t_status *)malloc(sizeof(t_status) * data->num_of_philo)))
+		return 0;
+	if(!(forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * data->num_of_philo)))
+		return 0;	
+
 	if(ac == 5)
-		philo->option = OPTION_OFF;
+		status->option = OPTION_OFF;
 	else
-		philo->option = OPTION_ON;
-	
-	philo->number_of_philosophers = philo_atoi(av[1]);
-	philo->time_to_die = philo_atoi(av[2]);
-	philo->time_to_eat = philo_atoi(av[3]);
-	philo->time_to_sleep = philo_atoi(av[4]);
+		status->option = OPTION_ON;
 
-	philo->eat_count = philo_atoi(av[ac - 1]);
-	
-	/*if(philo->option)
-		philo->eat_count = philo_atoi(av[5]);
-	else
-		philo->eat_count = 0;
-	*/
-		int i = 0;
-
-	philo->status = status;
-
-	while(i < philo->number_of_philosophers)
+	status->forks_free = forks;
+	int i = 0;
+	while(i < data->num_of_philo)
 	{
-		philo->status[i].die = 0;
-		philo->status[i].hungry = 0;
+		status[i].idx = i + 1;
+		status[i].data = data;
+		pthread_mutex_init(&(forks[i]), NULL);
 		++i;
 	}
-	return (philo);
+	i = 0;
+	while(i < data->num_of_philo)
+	{	
+		if(i == 0)
+		{
+			status[i].l_fork = &(forks[data->num_of_philo - 1]);
+			status[i].r_fork = &(forks[0]);
+		}
+		else
+		{	
+			status[i].l_fork = &(forks[i - 1]);
+			status[i].r_fork = &(forks[i]);
+		}
+		i++;	
+	}
+	return (status);
+}
+
+void *print(void *arg)
+{
+	t_status *status = (t_status *)arg;
+
+	printf("idx :%d\n", status->idx);
+	printf("forks : %p %p\n", status->l_fork, status->r_fork);
+	printf("option : %d\n", status->option);
+	
+	printf("num : %d\n", status->data->num_of_philo);
+	printf("die : %d\n", status->data->time_to_die);
+	printf("eat : %d\n", status->data->time_to_sleep);
+	printf("sleep : %d\n", status->data->time_to_sleep);
+	printf("count : %d\n", status->data->eat_count);
+
+	printf("\n\n");
+	return 0;
 }
 	
 int main(int argc, char *argv[])
 {
-	t_philo *philo;
+	t_status *status;
 
 	if(argc != 5 && argc != 6)
-		error_print();
-	
-	philo =	philo_init(argc, argv);
+		error_print(1);
+	status = status_init(argc, argv);
 
 
-	printf("%d\n", philo->option);
-	printf("philo numbers :%d\n die time : %d\n eat time : %d\n sleep time : %d\n eat count : %d\n", 
-			philo->number_of_philosophers, philo->time_to_die, 
-			philo->time_to_eat, philo->time_to_sleep, philo->eat_count);
-	
-	int i = 0;	
-	while(i < philo->number_of_philosophers)
+	//포크
+	int i = 0;
+	while(i < status->data->num_of_philo)
 	{
-		printf(" %d : die : %d  hungry : %d\n", i, philo->status[i].die ,philo->status[i].hungry); 
-		++i;
+		pthread_create(&(status[i].philo), NULL, &print, &status[i]);
+		pthread_join(status[i].philo, NULL);
+		i++;
 	}
 }
--- 3.philosopher/philo/main.h
+++ 3.philosopher/philo/main.h
@@ -13,29 +13,34 @@
 #ifndef MAIN_H
 # define MAIN_H
 
+#include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #define OPTION_ON	1
 #define OPTION_OFF	0
 
 typedef int t_bool;
 
+typedef struct	s_data
+{
+	int					num_of_philo;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					eat_count;
+}				t_data;
+
 typedef struct 	s_status
 {
-	int die;
-	int hungry;
+	int					idx;
+	pthread_t			philo;
+
+	pthread_mutex_t		*forks_free;	
+	pthread_mutex_t		*l_fork;
+	pthread_mutex_t		*r_fork;
+	t_bool				option;
+	t_data				*data;
 }				t_status;
-
-typedef struct	s_philo
-{
-	int		number_of_philosophers;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	t_bool	option;
-	int		eat_count;
-	t_status *status;
-
-}				t_philo;
 
 #endif
