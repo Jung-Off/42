f0daa538594ba0b1805e022edf77ec190378258f
lewis941001 <lewis1004@naver.com>
2021-09-14 22:33:32 +0900

    [210914_fix(reduce)]put_fork(pointer)

--- 3.philosopher/philo_copy/main.c
+++ 3.philosopher/philo_copy/main.c
@@ -12,15 +12,15 @@
 
 #include "main.h"
 
-void error_print(int err_num)
+void	error_print(int err_num)
 {
-	if(err_num == 1)
+	if (err_num == 1)
 		printf("error argc count\n");
-	else if(err_num == 2)
+	else if (err_num == 2)
 		printf("error you input char\n");
-	else if(err_num == 3)
+	else if (err_num == 3)
 		printf("error input out of range\n");
-	else if(err_num == 4)
+	else if (err_num == 4)
 		printf("error malloc assignment\n");
 	exit(1);
 }
@@ -50,7 +50,7 @@ int	philo_isspace(char ch)
 	return (0);
 }
 
-int philo_atoi(char *str)
+int	philo_atoi(char *str)
 {
 	int			isnum;
 	int			minus;
@@ -69,7 +69,7 @@ int philo_atoi(char *str)
 	}
 	while (*str)
 	{
-		if(!philo_isdigit(*str))
+		if (!philo_isdigit(*str))
 			error_print(2);
 		num = num * 10 + (*(str++) - '0');
 		isnum = 1;
@@ -81,70 +81,73 @@ int philo_atoi(char *str)
 		error_print(3);
 	return (num);
 }
-	
-int make_philo(t_philo **philo, t_info info)
+
+t_philo	*make_philo(t_info info)
 {
-	*philo = (t_philo *)malloc(sizeof(t_philo) * info.num_to_philo);
-	if (*philo == NULL)
-		return (1);
-	else
-		return (0);
+	t_philo	*philo;
+
+	philo = (t_philo *)malloc(sizeof(t_philo) * info.num_to_philo);
+	if (philo == NULL)
+		error_print(4);
+	return (philo);
 }
 
-int make_forks(pthread_mutex_t **forks, t_info info)
+pthread_mutex_t	*make_forks(t_info info)
 {
-	*forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * info.num_to_philo);
-	if (*forks == NULL)
-		return (1);
-	else
-		return (0);
+	pthread_mutex_t	*forks;
+
+	forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t)
+			* info.num_to_philo);
+	if (forks == NULL)
+		error_print(4);
+	return (forks);
 }
 
-void put_the_fork(t_philo **philo, t_info info, pthread_mutex_t *forks)
+void	put_the_fork(t_philo *philo, t_info info, pthread_mutex_t *forks)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (i < info.num_to_philo)
 	{	
 		if (i == 0)
 		{
-			(*philo)[i].l_fork = &(forks[info.num_to_philo - 1]);
-			(*philo)[i].r_fork = &(forks[0]);
+			philo[i].l_fork = &(forks[info.num_to_philo - 1]);
+			philo[i].r_fork = &(forks[0]);
 		}
 		else
 		{
-			(*philo)[i].l_fork = &(forks[i - 1]);
-			(*philo)[i].r_fork = &(forks[i]);
+			philo[i].l_fork = &(forks[i - 1]);
+			philo[i].r_fork = &(forks[i]);
 		}
 		++i;
 	}
 }
 
-void init_thread(t_philo **philo, t_info info)
+t_philo	*init_thread(t_info info)
 {
-	int i;
-	pthread_mutex_t *forks;
+	int				i;
+	t_philo			*philo;
+	pthread_mutex_t	*forks;
 
 	i = 0;
-	if (make_philo(philo, info))
-		return (error_print(4));
-	if (make_forks(&forks, info))
-		return (error_print(4));
+	philo = make_philo(info);
+	forks = make_forks(info);
 	while (i < info.num_to_philo)
 	{
-		(*philo)[i].idx = i + 1;
+		philo[i].idx = i + 1;
 		pthread_mutex_init(&(forks[i]), NULL);
-		(*philo)[i].info = &info;
+		philo[i].info = &info;
 		++i;
 	}
-	(*philo)->fork = forks;
+	philo->fork = forks;
 	put_the_fork(philo, info, forks);
+	return (philo);
 }
 
-void init_info(int ac, char *argv[], t_info *info)
+void	init_info(int ac, char *argv[], t_info *info)
 {
-	if(ac == 5)
+	if (ac == 5)
 		info->option = OPTION_OFF;
 	else
 		info->option = OPTION_ON;
@@ -155,44 +158,38 @@ void init_info(int ac, char *argv[], t_info *info)
 	info->num_of_eat = philo_atoi(argv[ac - 1]);
 }
 
-void *print(void *arg)
+void	*print(void *arg)
 {
 	t_philo *status = (t_philo *)arg;
 
 	printf("idx :%d\n", status->idx);
 	printf("forks : %p %p\n", status->l_fork, status->r_fork);
 	//printf("option : %d\n", status->info->option);
-
 	printf("num : %d\n", status->info->num_to_philo);
 	printf("die : %d\n", status->info->time_to_die);
 	printf("eat : %d\n", status->info->time_to_eat);
 	printf("sleep : %d\n", status->info->time_to_sleep);
 	printf("count : %d\n", status->info->num_of_eat);
-
 	printf("\n\n");
-	return 0;
+	return (0);
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	t_philo *philo;
+	int		i;
+	t_philo	*philo;
 	t_info	info;
-	int i;
 
-	if(argc != 5 && argc != 6)
+	i = 0;
+	if (argc != 5 && argc != 6)
 		error_print(1);
 	init_info(argc, argv, &info);
-	
-	init_thread(&philo, info);
-	
-	i = 0;
-	while(i < info.num_to_philo)
+	philo = init_thread(info);
+	while (i < info.num_to_philo)
 	{
 		pthread_create(&(philo[i].thread), NULL, &print, &philo[i]);
 		pthread_join(philo[i].thread, NULL);
 		++i;
 	}
-
-	//start(philo);
-
+	//free fork philo//
 }
--- 3.philosopher/philo_copy/main.h
+++ 3.philosopher/philo_copy/main.h
@@ -13,32 +13,32 @@
 #ifndef MAIN_H
 # define MAIN_H
 
-#include <pthread.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#define OPTION_ON	1
-#define OPTION_OFF	0
+# include <pthread.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# define OPTION_ON	1
+# define OPTION_OFF	0
 
-typedef int t_bool;
+typedef int	t_bool;
 
-typedef struct	s_info
+typedef struct s_info
 {
-	int num_to_philo;
-	int time_to_die;
-	int time_to_eat;
-	int time_to_sleep;
-	t_bool			option;
-	int num_of_eat;
+	int		num_to_philo;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	t_bool	option;
+	int		num_of_eat;
 }				t_info;
 
 typedef struct s_philo
 {
 	int				idx;
 	pthread_t		thread;
-	pthread_mutex_t *fork;
-	pthread_mutex_t *l_fork;
-	pthread_mutex_t *r_fork;
+	pthread_mutex_t	*fork;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*r_fork;
 
 	t_info			*info;
 }				t_philo;
