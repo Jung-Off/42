5581a335c4869fcdf7a043a8432e586dd1692292
lewis941001 <lewis1004@naver.com>
2022-01-08 23:31:19 +0900

    [220108_add]cpp01_ex02

--- 4.cpp_modules/cpp_module01/ex02/main.cpp
+++ 4.cpp_modules/cpp_module01/ex02/main.cpp
@@ -1,1 +1,15 @@
-.
+#include <iostream>
+
+int main()
+{
+    std::string str("HI THIS IS BRAIN");
+
+    std::string *stringPTR = &str;
+    std::string &stringREF = *stringPTR;
+
+    std::cout << "stringPTR Address : " << stringPTR << std::endl;
+    std::cout << "stringREF Address : " << &stringREF << std::endl;
+
+    std::cout << "stringPTR contents : " << *stringPTR << std::endl;
+    std::cout << "stringREF contents : " << stringREF << std::endl;
+}
