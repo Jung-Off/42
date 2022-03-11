738b8194400e5118b625caf9bbcc33e91a524b94
lewis941001 <lewis1004@naver.com>
2022-03-12 00:39:48 +0900

    [220312_cpp07]ex01

--- 4.cpp_modules/cpp_subject/CPP_Module_07/ex01/iter.hpp
+++ 4.cpp_modules/cpp_subject/CPP_Module_07/ex01/iter.hpp
@@ -3,6 +3,18 @@
 
 #include <iostream>
 
+template <typename T>
+void iter(T *target, int length, void (*f)(const T&))
+{
+    for (int i = 0; i < length; i++)
+        f(target[i]);
+    std::cout << std::endl;
+}
 
+template <typename T>
+void print(const T& a)
+{
+    std::cout << a << " ";
+}
 
 #endif
--- 4.cpp_modules/cpp_subject/CPP_Module_07/ex01/main.cpp
+++ 4.cpp_modules/cpp_subject/CPP_Module_07/ex01/main.cpp
@@ -3,5 +3,21 @@
 
 int main()
 {
+    int         age[10] = {20, 21, 22, 23, 24, 25, 26, 27, 28, 29};
+    char        alpabet[7] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
+    double      rate[4] = {3.1415, 2.13748, 1.41414, 2.76543};
+    std::string fruits[3] = {"apple", "banana", "egg"};
+
+    std::cout << "int : ";
+    iter(age, sizeof(age) / sizeof(*age), print);
+
+    std::cout << "char : ";
+    iter(alpabet, sizeof(alpabet) / sizeof(*alpabet), print);
+
+    std::cout << "double : ";
+    iter(rate, sizeof(rate) / sizeof(*rate), print);
+
+    std::cout << "string : ";
+    iter(fruits, sizeof(fruits) / sizeof(*fruits), print);
     
 }
